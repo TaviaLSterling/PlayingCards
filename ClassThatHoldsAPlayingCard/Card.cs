f922974e01f27afa9dc59ec841d2c5bf341e0cd3
Tavia Sterling <TaviaSterling@outlook.com>
2019-02-26 12:43:21 -0700

    suits class

--- ClassThatHoldsAPlayingCard/Card.cs
+++ ClassThatHoldsAPlayingCard/Card.cs
@@ -8,5 +8,17 @@ namespace ClassThatHoldsAPlayingCard
 {
     class Card
     {
+        public Suits Suit { get; set; }
+        public Values Value { get; set; }
+
+        public Card(Suits suit, Values value)
+        {
+            this.Suit = suit;
+            this.Value = value;
+        }
+        public string Name
+        {
+            get { return Value.ToString() + " of " + Suit.ToString(); }
+        }
     }
 }
